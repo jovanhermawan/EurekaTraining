c17a7b4c2050eaae689c86a4a9cf39e2d1061009
Jovan Hermawan <49465450+jovanhermawan@users.noreply.github.com>
2022-01-17 11:00:03 +0800

    Delete ROS directory

--- ROS/new_workspace/build/node_basics_p/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jovanhermawan/Desktop/new_workspace/src/node_basics_p
-# Build directory: /home/jovanhermawan/Desktop/new_workspace/build/node_basics_p
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ROS/new_workspace/src/package1/src/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int main()
-{
-	std::cout << "Hello from C++ using cout \n";
-	printf("Hello from C++ using printf \n");
-}
